116976e66daaa9c0f4be3615377e58f199b42370
Ben <39630191+drachen2299@users.noreply.github.com>
2021-05-03 09:35:07 -0600

    update added chart.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const alertBanner = document.getElementById("alert");
+const trafficCanvas = document.getElementById("traffic-chart");
+const dailyCanvas = document.getElementById("daily-chart");
+const mobileCanvas = document.getElementById("mobile-chart");
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
 
 alertBanner.innerHTML =
 `
@@ -9,7 +15,105 @@ alertBanner.innerHTML =
 `
 alertBanner.addEventListener('click', e => {
     const element = e.target;
-    if (element.classlist.contains("alert-banner-close")) {
-        alert.style.display = "none";
+    if (element.classList.contains("alert-banner-close")) {
+        alertBanner.style.display = "none";
     }
+});
+
+
+
+// chart.js usage
+
+    //Line Chart
+let trafficsData = {
+    labels:["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+    }]
+};
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line',
+    data: trafficsData,
+    options: trafficOptions
+});
+
+    //Bar Chart
+let dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: "# of Hits",
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: "#7477BF",
+        borderWidth: 1
+    }]
+};
+
+let dailyOptions = {
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+            display: false
+        }
+    }
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: "bar",
+    data: dailyData,
+    options: dailyOptions
+});
+
+    //Mobile Chart
+let mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: "# of Users",
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            "#7477BF",
+            "#78CF82",
+            "#51B6C8"
+        ]
+    }]
+};
+
+let mobileOptions = {
+    plugins: {
+        legend: {
+            position: "right",
+            labels: {
+                boxWidth: 10,
+                fontStyle: "bold"
+            }
+        }
+    }
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: "doughnut",
+    data: mobileData,
+    options: mobileOptions
 });
