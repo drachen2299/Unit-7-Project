588bcbede8fbae8f15b1aadd782732126b2eefb5
Ben <39630191+drachen2299@users.noreply.github.com>
2021-06-21 13:09:34 -0600

    Found Project

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ alertBanner.addEventListener('click', e => {
     }
 });
 
+alertBanner.style.display = "inline";
+alertBanner.style.backgroundColor = "#846cc4";
 
 
 // chart.js usage
@@ -29,7 +31,7 @@ let trafficsData = {
     labels:["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-        backgroundColor: 'rgba(116, 119, 191, .3)',
+        backgroundColor: "rgba(116, 119, 191, .3)",
         borderWidth: 1,
     }]
 };
